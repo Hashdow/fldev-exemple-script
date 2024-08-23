da6505b272762fb552dd326d76b4cae08b41d943
hashdow <contact.wpac@gmail.com>
2024-08-23 09:31:19 +0200

    Script addToCart button listerner

--- monScript.js
+++ monScript.js
@@ -1,1 +1,23 @@
-alert('Script Tag injecté avec succès !');
+document.addEventListener('DOMContentLoaded', function () {
+    const addToCartButton = document.querySelector('button[name="add"]');
+    const imageInput = document.getElementById('uploaded-image-url');
+    const imageUploadButton = document.getElementById('image-input-fldev');
+
+    console.log('DOM fully loaded and parsed');
+    console.log('addToCart button: ', addToCartButton);
+    console.log('imageInput: ', imageInput);
+    console.log('imageUploadButton: ', imageUploadButton);
+
+    // Vérifier si le bouton d'ajout d'image est présent
+    if (imageUploadButton) {
+        // Ajouter un écouteur d'événement au bouton "Ajouter au panier"
+        addToCartButton.addEventListener('click', function (event) {
+            // Vérifier si l'image a bien été ajoutée (vérifie si l'input caché a une valeur)
+            if (!imageInput.value) {
+                // Bloquer l'ajout au panier et afficher une alerte
+                event.preventDefault();
+                alert("Veuillez ajouter une image avant d'ajouter le produit au panier.");
+            }
+        });
+    }
+});
